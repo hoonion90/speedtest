3f5b53c48ec03246cc7ec33085971d392875b747
hoonion90 <clearch21@gmail.com>
2021-10-20 21:30:56 +0900

    Modify Index

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', function (request, response) {
+  response.render('./public/index.ejs');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
