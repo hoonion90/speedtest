8e26a2a6a2699f89d35171a18c6a4ae8180befdc
hoonion90 <clearch21@gmail.com>
2021-10-20 21:36:24 +0900

    Install EJS

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const ejs = require('ejs');
 
 app.get('/', function (request, response) {
   response.render('./public/index.ejs');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
