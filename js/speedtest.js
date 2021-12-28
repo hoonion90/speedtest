fc5f213c0516d4ea6bd8e787d02fb8e8cafae9aa
hoonion90 <clearch21@gmail.com>
2021-12-28 11:43:57 +0900

    타이머 구현

--- js/speedtest.js
+++ js/speedtest.js
@@ -3,7 +3,10 @@ let $ready;
 let $click;
 let $result;
 let $caution;
-let timer = 0;
+let timer;
+let time = 0;
+let count = 0;
+let sumTime = 0;
 
 window.onkeydown = (e) => {
     $start = $("#gameStart");
@@ -16,6 +19,18 @@ window.onkeydown = (e) => {
     // if(e.key == "f" || e.key == "d"){
     //     console.log(e.key);
     // }
+    if(e.key == "d" || e.key == "f" && $click.hasClass("active")){
+        inActive($click);
+        active($result);
+        timerStop();
+        console.log(time);
+        time = 0;
+    }
+    if(e.key == "Enter" && $result.hasClass("active")){
+        inActive($result);
+        active($ready);
+        setReady();
+    }
     if(e.key =="Enter" && $start.hasClass("active")){
         inActive($start);
         active($ready);
@@ -37,5 +52,29 @@ function active(element){
     element.addClass("active");
 }
 function setReady(){
-    
+    let rand = getRandomArbitrary();
+    console.log(rand);
+    time = 0;
+    setTimeout(()=>{
+        inActive($ready);
+        active($click);
+        setClick();
+    },rand * 500);
+}
+function setClick(){
+    timerStart();
+}
+function getRandomArbitrary() {
+    return Math.round(Math.random() * (12 - 3) + 3);
+}
+
+function timerStart(){
+	timer = window.setInterval(clockRunning, 10);
+}
+function timerStop(){
+	window.clearInterval(timer);
+}
+function clockRunning(){
+    console.log(time);
+    return time ++;
 }
