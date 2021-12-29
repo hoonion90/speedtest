eefaa5816d9f67fda906b08349d8805f44bd7147
hoonion90 <clearch21@gmail.com>
2021-12-29 21:31:22 +0900

    타이머 기능 구현

--- js/speedtest.js
+++ js/speedtest.js
@@ -3,10 +3,13 @@ let $ready;
 let $click;
 let $result;
 let $caution;
+let $count;
 let timer;
+let timer2;
 let time = 0;
 let count = 0;
 let sumTime = 0;
+let startTime;
 
 window.onkeydown = (e) => {
     $start = $("#gameStart");
@@ -14,32 +17,51 @@ window.onkeydown = (e) => {
     $click = $("#gameClick");
     $result = $("#gameResult");
     $caution = $("#gameCaution");
-
+    $count = $("#count");
     console.log(e.key);
     // if(e.key == "f" || e.key == "d"){
     //     console.log(e.key);
     // }
-    if(e.key == "d" || e.key == "f" && $click.hasClass("active")){
+    if((e.key == "d" || e.key == "f") && $click.hasClass("active")){
         inActive($click);
         active($result);
         timerStop();
         console.log(time);
+        $count.text(time);
+        if(count <= 5) {
+            sumTime += time;
+            count ++;
+        }
+        if(count == 5){
+            $("#testResult").val( Math.round(sumTime / 5) );
+            console.log(Math.round(sumTime / 5));
+            //document.mainx.submit();
+        }
+    }
+    else if( (e.key == "d" || e.key == "f") && $ready.hasClass("active")){
+        inActive($ready);
+        active($caution);
+        window.clearTimeout(timer2);
+        timerStop();
         time = 0;
     }
-    if(e.key == "Enter" && $result.hasClass("active")){
+    else if(e.key == "Enter" && $result.hasClass("active")){
         inActive($result);
         active($ready);
         setReady();
+        time = 0;
     }
-    if(e.key =="Enter" && $start.hasClass("active")){
+    else if(e.key =="Enter" && $start.hasClass("active")){
         inActive($start);
         active($ready);
         setReady();
+        time = 0;
     }
-    if(e.key =="Enter" && $caution.hasClass("active")){
+    else if(e.key =="Enter" && $caution.hasClass("active")){
         inActive($caution);
         active($ready);
         setReady();
+        time = 0;
     }
 };
 
@@ -53,9 +75,9 @@ function active(element){
 }
 function setReady(){
     let rand = getRandomArbitrary();
-    console.log(rand);
+    // console.log(rand);
     time = 0;
-    setTimeout(()=>{
+    timer2 = setTimeout(()=>{
         inActive($ready);
         active($click);
         setClick();
@@ -69,12 +91,16 @@ function getRandomArbitrary() {
 }
 
 function timerStart(){
-	timer = window.setInterval(clockRunning, 10);
+	clockRunning();
 }
 function timerStop(){
 	window.clearInterval(timer);
 }
 function clockRunning(){
-    console.log(time);
-    return time ++;
+    startTime = Date.now();
+    timer = setInterval(function() {
+        let elapsedTime = Date.now() - startTime;
+        // time = (elapsedTime / 1000).toFixed(3);
+        time = elapsedTime;
+    }, 1);
 }
